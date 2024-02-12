784c0df533912ffc570a5713e8aebaeb13e9f91d
Alexander Toch <alexander.toch@student.tugraz.at>
2024-02-12 09:29:03 +0100

    change to culane model

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 ONNX_MODEL_PATH = "../resnet50_resa_tusimple_20211019.onnx"
 BENCHMARK = False
 
-input_sizes = (360, 640)  # defined in the pretrained model
+# input_sizes = (360, 640)  # defined in the pretrained model
+input_sizes = (288, 800)  # defined in the pretrained model
 max_lane = 4
 gap = 10
 ppl = 56
