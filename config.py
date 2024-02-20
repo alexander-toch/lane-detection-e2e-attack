15ac04bf11b5339e973d25b82cc587a7abb07d1c
Alexander Toch <alexander.toch@student.tugraz.at>
2024-02-20 17:31:20 +0100

    fix lane fitting

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-ONNX_MODEL_PATH = "../resnet50_resa_tusimple_20211019.onnx"
-# ONNX_MODEL_PATH = "../resnet50_resa_culane_20211016.onnx"
+# ONNX_MODEL_PATH = "../resnet50_resa_tusimple_20211019.onnx"
+ONNX_MODEL_PATH = "../resnet50_resa_culane_20211016.onnx"
 BENCHMARK = False
 
-input_sizes = (360, 640)  # tusimple
-# input_sizes = (288, 800)  # culane
+# input_sizes = (360, 640)  # tusimple
+input_sizes = (288, 800)  # culane
 max_lane = 4
 gap = 10
 ppl = 56
