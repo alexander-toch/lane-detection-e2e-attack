6e6c968a68d1e92213decca6db5951a4f1ab80c5
Alexander Toch <alexander.toch@student.tugraz.at>
2024-05-13 09:25:57 +0200

    fix dummy env for patch rendering

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 import sys
 from metadrive import MetaDriveEnv
+from metadrive.component.sensors.rgb_camera import RGBCamera
 
 def dummy_env():
-    env = MetaDriveEnv({"use_render": False, "image_observation": False})
+    env = MetaDriveEnv({"use_render": False, "image_observation": True, "sensors": {"rgb_camera": (RGBCamera, 100, 100)}})
     try:
         env.reset()
-        for i in range(1, 100):
+        for i in range(1, 10):
             o, r, tm, tc, info = env.step([0, 1])
     except:
         print("Error happens in Bullet physics world !")
