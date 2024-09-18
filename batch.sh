a686819435868b6268342cc972ea1b03ec4250d1
Alexander Toch <alexander.toch@gmail.com>
2024-09-18 13:57:20 +0200

    add notebooks and data

--- batch.sh
+++ batch.sh
@@ -1,21 +1,8 @@
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=1020
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=1040
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=1060
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=1080
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=2020
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=2040
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=2060
-python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=2080
-python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml
-python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=3020
-python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=3040
-python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=3060
-python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=3080
-python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml
-python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=4020
-python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=4040
-python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=4060
-python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=4080
-
+python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=100 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_resa.yml --seed=125 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=200 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_resnet50.yml --seed=225 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=300 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_erfnet.yml --seed=325 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=400 --num_scenarios=25
+python simulator.py --config=config/attack_2_pass_evaluate_custom_scnn.yml --seed=425 --num_scenarios=25
